144ad0767a6d66fd54dc1112c6948a430bc03d48
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-09-26 14:30:09 +0000

    feat: added gruvbox colour scheme to neovim

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard", -- or "soft", "medium"
+				transparent_mode = false,
+			})
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+}
